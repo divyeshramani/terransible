2023d2adf75cb58646d8edd34189950e3d6300ce
Divyesh Ramani <divyesh.ramani@disney.com>
2019-03-15 18:40:31 -0700

    Rename file and added bootstraping of ssh agent

--- playground/playbooks/ansible_init.sh
+++ playground/playbooks/ansible_init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Setup SSH Agent
+eval `ssh-agent`
+ssh-add ~/.ssh/kryptonite
+
+# Merge Hosts File
 rm -f hosts
 tail -n +1 hosts_* | sed  -e 's/==> /[/g' | sed -e 's/ <==/]/g' | sed -e 's/\[hosts_/\[/g' > hosts
 rm -f hosts_*
